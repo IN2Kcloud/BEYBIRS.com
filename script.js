92ee8be066fc9c51736dfcffd391fc70557d6ea0
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-06-21 12:38:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', () => {
+  document.body.classList.remove('before-load');
+});
+document.querySelector('.loading').addEventListener('transitionend', (e) => {
+  document.body.removeChild(e.currentTarget);
+});
+
 
 //CARDS
 document.getElementById("cards").onmousemove = e => {
