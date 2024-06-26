c5b00434e74a5e563673853d102c1b513eb8b6d4
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-06-26 23:31:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.querySelector('.loading').addEventListener('transitionend', (e) => {
   document.body.removeChild(e.currentTarget);
 });
 
-
 //CARDS
 document.getElementById("cards").onmousemove = e => {
   for(const card of document.getElementsByClassName("card")) {
@@ -17,3 +16,58 @@ document.getElementById("cards").onmousemove = e => {
     card.style.setProperty("--mouse-y", `${y}px`);
   };
 }
+
+//SERVICES
+
+var navLink = gsap.utils.toArray(".nav-link"),
+    imgWrap = document.querySelector('.img-wrapper'),
+    imgItem = document.querySelector('.img-placeholder img');
+
+
+function moveImg(e){
+    var mouseX = e.clientX,
+        mouseY = e.clientY
+    tl = gsap.timeline();
+    tl.to(imgWrap, {
+        duration:1,
+        x: mouseX,
+        y: mouseY,
+        ease: Expo.ease
+    })
+}
+
+function linkHover(e){
+    if (e.type === "mouseenter"){
+        var imgSrc = e.target.dataset.src;
+        var tl = gsap.timeline();
+
+        tl.set(imgItem, {
+            attr: {src : imgSrc}
+        }).to(imgWrap, {
+            autoAlpha:1,
+            scale:1
+        });
+    } else if (e.type === "mouseleave"){
+        var tl = gsap.timeline();
+        tl.to(imgWrap, {
+            autoAlpha:0,
+            scale:0.3
+        })
+    }
+}
+
+function initAnimation(){
+    navLink.forEach(link => {
+        link.addEventListener('mouseenter', linkHover);
+        link.addEventListener('mouseleave', linkHover);
+        link.addEventListener('mousemove', moveImg);
+    })
+}
+
+function init(){
+    initAnimation();
+}
+
+window.addEventListener("load", function(){
+    init();
+})
