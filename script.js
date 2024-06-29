754c29718755bc80e4d6781e79a8a1b51eb3f36d
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-06-29 20:00:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -23,17 +23,19 @@ var navLink = gsap.utils.toArray(".nav-link"),
     imgWrap = document.querySelector('.img-wrapper'),
     imgItem = document.querySelector('.img-placeholder img');
 
-
 function moveImg(e){
     var mouseX = e.clientX,
-        mouseY = e.clientY
-    tl = gsap.timeline();
+        mouseY = e.clientY;
+    var imgWrapWidth = imgWrap.offsetWidth,
+        imgWrapHeight = imgWrap.offsetHeight;
+        
+    var tl = gsap.timeline();
     tl.to(imgWrap, {
-        duration:1,
-        x: mouseX,
-        y: mouseY,
+        duration: 1,
+        x: mouseX - imgWrapWidth / 2,
+        y: mouseY - imgWrapHeight / 2,
         ease: Expo.ease
-    })
+    });
 }
 
 function linkHover(e){
@@ -44,15 +46,15 @@ function linkHover(e){
         tl.set(imgItem, {
             attr: {src : imgSrc}
         }).to(imgWrap, {
-            autoAlpha:1,
-            scale:1
+            autoAlpha: 1,
+            scale: 1
         });
     } else if (e.type === "mouseleave"){
         var tl = gsap.timeline();
         tl.to(imgWrap, {
-            autoAlpha:0,
-            scale:0.9
-        })
+            autoAlpha: 0,
+            scale: 0.9
+        });
     }
 }
 
@@ -61,7 +63,7 @@ function initAnimation(){
         link.addEventListener('mouseenter', linkHover);
         link.addEventListener('mouseleave', linkHover);
         link.addEventListener('mousemove', moveImg);
-    })
+    });
 }
 
 function init(){
@@ -70,4 +72,4 @@ function init(){
 
 window.addEventListener("load", function(){
     init();
-})
+});
