97fd0a4803fc005d7e5c0fe48bfd026f14334fe5
SINXSIN <131200320+IN2Kcloud@users.noreply.github.com>
2024-07-01 21:10:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -6,17 +6,47 @@ document.querySelector('.loading').addEventListener('transitionend', (e) => {
 });
 
 //CARDS
-document.getElementById("cards").onmousemove = e => {
-  for(const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-          x = e.clientX - rect.left,
-          y = e.clientY - rect.top;
+// Function to update mouse coordinates
+function updateMouseCoordinates(e, card) {
+  const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
 
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
-  };
+  card.style.setProperty("--mouse-x", `${x}px`);
+  card.style.setProperty("--mouse-y", `${y}px`);
 }
 
+// Function to update touch coordinates
+function updateTouchCoordinates(e, card) {
+  const touch = e.touches[0],
+        rect = card.getBoundingClientRect(),
+        x = touch.clientX - rect.left,
+        y = touch.clientY - rect.top;
+
+  card.style.setProperty("--mouse-x", `${x}px`);
+  card.style.setProperty("--mouse-y", `${y}px`);
+}
+
+// Handle mouse move and touch move events
+document.getElementById("cards").addEventListener("mousemove", e => {
+  for (const card of document.getElementsByClassName("card")) {
+    updateMouseCoordinates(e, card);
+  }
+});
+
+document.getElementById("cards").addEventListener("touchmove", e => {
+  for (const card of document.getElementsByClassName("card")) {
+    updateTouchCoordinates(e, card);
+  }
+});
+/*
+  document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+      card.classList.toggle("clicked");
+    });
+  });
+*/
+  
 //SERVICES
 
 var navLink = gsap.utils.toArray(".nav-link"),
